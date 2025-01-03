e65c191c61957ee7e8cab3749ed22b0229baedbe
HyeongKeunHong <whaihong@g.skku.edu>
2025-01-04 06:45:08 +0900

    Comment: motor_test.ino

--- src/control/motor_test/motor_test.ino
+++ src/control/motor_test/motor_test.ino
@@ -1,11 +1,13 @@
 //모터 제어 코드
 
+//모터의 회전 속도 입력 (-255 ~ 255)
+//양수는 전진, 음수는 후진
+//절댓값이 클수록 모터가 빠르게 회전 
+static int target_speed = 50;
+
 //모터 드라이버 IN1,IN2에 연결하는 아두이노 핀번호 입력 (모터가 반대로 동작하면 순서를 IN2,IN1 순서로 입력)
 static int test_motor1 = 3;
 static int test_motor2 = 4;
-
-//모터의 회전 속도 입력 (-255 ~ 255)
-static int target_speed = 50;
 
 
 //여기서부터는 건드릴 필요 없음.
