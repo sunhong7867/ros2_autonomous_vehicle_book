feefc56de1b91fd7cfb7f8c60e32880727967259
HyeongKeunHong <whaihong@g.skku.edu>
2025-01-15 02:35:37 +0900

    Update: serial_communication_pkg

--- src/serial_communication_pkg/serial_communication_pkg/lib/convert_arduino_msg.py
+++
@@ -1,6 +0,0 @@
-def convert_to_serial_message(steering_command, left_speed_command, right_speed_command):
-    return (str(steering_command+7).zfill(2) + 
-            str(left_speed_command + 255).zfill(3) + 
-            str(right_speed_command + 255).zfill(3) + 
-            '\n')
-
